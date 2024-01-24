a997b83c3b4ae76004af2b1e6034cd1d1b2b85ca
AVLKarnaukh <karnauhkgb@mail.ru>
2024-01-25 00:26:26 +0700

    Change pom.xml

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMaxIfLess() {
         StatisticsService service = new StatisticsService();
